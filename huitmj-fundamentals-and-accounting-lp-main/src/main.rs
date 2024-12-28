877b4fb5d187b181adce2b52d65e4c95b504339a
James Hui <huitmj@gmail.com>
2024-12-28 16:13:38 +0000

    Update main.rs

--- huitmj-fundamentals-and-accounting-lp-main/src/main.rs
+++ huitmj-fundamentals-and-accounting-lp-main/src/main.rs
@@ -15,6 +15,8 @@ enum AccountingError {
 #[derive(Debug)]
 pub enum Tx {
     // Add variants for storing withdraw/deposit transactions
+    Deposit{account: String, amount: u64},
+    Withdraw{account: String, amount: u64},
 }
 
 /// A type for managing accounts and their current currency balance
